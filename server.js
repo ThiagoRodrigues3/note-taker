b5c29db7d2afc10359d4daa8a2ddeaa9145d24c7
Thiago Rod <whytigo@gmail.com>
2022-03-13 17:07:43 -0400

    added notes to Server.js and added API routes file and fsUtils file

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
+// brings in Express to handle all HTTP calls
 const express = require('express')
-const app = express()
+// used to help handle file paths
 const path = require('path')
+// creating shortcut to organize API calls
 const api = require('routes/api.js')
 
+// This is used to process the PORT for either Heroku or Local machine
 const PORT = process.env.PORT || 3001
 
+// creates Express as an object
+const app = express()
+
+// Used to help express read and understand JSON and URLs
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+// Makes public folders as the Static files
 app.use(express.static('public'))
 
+// Sets the API path
 app.use('/api', api)
 
+// Shows note page when called
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '.public.notes.html'))
 })
 
+// for any other call takes you home instead of erroring.
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// Set app to listen to PORT chosen above
 app.listen(PORT, () =>
 	console.log(`Listening at http://localhost:${PORT}`)
 );
